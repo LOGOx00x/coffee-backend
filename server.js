dc10b96beca1a0f86283b07ba1aedfc8e1c885ff
LOGOx00x <bubpawong2543@gmail.com>
2026-03-29 14:08:07 +0700

    docker and crud nosql

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import cors from "cors";
 import express from "express";
 
+import crud_menu_nosql from "./routes/crud_menu_nosql.js";
 import delete_menu from "./routes/delete_menu.js";
 import get_customer from "./routes/get_customer.js";
 import get_menu from "./routes/get_menu.js";
+import get_report_nosql from "./routes/get_report_nosql.js";
 import get_toppings from "./routes/get_toppings.js";
 import patch_menu from "./routes/patch_menu.js";
 import post_customer from "./routes/post_customer.js";
 import post_menu from "./routes/post_menu.js";
 import post_order from "./routes/post_order.js";
+import post_order_nosql from "./routes/post_order_nosql.js";
 import put_menu from "./routes/put_menu.js";
 
 const app = express();
@@ -24,13 +27,16 @@ app.use(get_customer);
 app.use(get_toppings);
 app.use(post_customer);
 app.use(post_order);
+app.use(crud_menu_nosql);
+app.use(post_order_nosql);
+app.use(get_report_nosql);
 
 app.listen(3000, () => {
-  console.log("Server กำลังทำงานที่ http://localhost:3000");
+    console.log("Server กำลังทำงานที่ http://localhost:3000");
 });
 
 app.get("/api/test", (req, res) => {
-  res.json({
-    status: "OK",
-  });
+    res.json({
+        status: "OK",
+    });
 });
